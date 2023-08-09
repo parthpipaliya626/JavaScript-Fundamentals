6f0e7ac301b05b628656ee8efe433186595da0b0
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-09 19:32:52 +0530

    square bracket

---
+++ 4/4.1/task_1.js
@@ -0,0 +1,22 @@
+// square bracket notation
+ /* 
+  one line for each other
+
+  create an empty object user.
+  set value in square brackets is true
+  get alert user value
+  delete user value
+ */
+
+
+
+let user = {};
+
+// set
+user["likes birds"] = true;
+
+// get
+alert(user["likes birds"]); // true
+
+// delete
+delete user["likes birds"];
