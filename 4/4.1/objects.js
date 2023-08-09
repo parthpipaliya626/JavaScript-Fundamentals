ab361cfc77422fdea231b29ca44b73fe10ee251c
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-09 19:26:07 +0530

    for in loop

--- 4/4.1/objects.js
+++ 4/4.1/objects.js
@@ -1,11 +1,9 @@
-
-
 // Property values are accessible using the dot notation
-<script>
- {/* get property values of the object: */}
+
+ // get property values of the object: 
 alert( user.name ); // John
 alert( user.age ); // 30
-</script>
+
 
 
 // To remove a property, we can use the delete operator:
@@ -13,17 +11,32 @@ delete user.age;
 
 // an alternative “square bracket notation” that works with any string:
 
-<script>
+
 "use strict";
 
 let user = {};
 
- {/* set */}
+ // set 
 user["likes birds"] = true;
 
- {/* get */}
+ // get 
 alert(user["likes birds"]); // true
 
- {/* delete */}
+ // delete 
 delete user["likes birds"];
-</script>
+
+
+// for in loop
+
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+  };
+  
+  for (let key in user) {
+    // keys
+    alert( key );  // name, age, isAdmin
+    // values for the keys
+    alert( user[key] ); // John, 30, true
+  }
