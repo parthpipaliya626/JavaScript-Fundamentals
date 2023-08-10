b3a48365b52c22cbf7af0e93b2cec72948a0d81e
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-10 16:58:26 +0530

    non-integer

--- 4/4.1/task_4.js
+++ 4/4.1/task_4.js
@@ -5,3 +5,18 @@
 alert( String(Math.trunc(Number("49"))) ); // "49", same, integer property
 alert( String(Math.trunc(Number("+49"))) ); // "49", not same "+49" ⇒ not integer property
 alert( String(Math.trunc(Number("1.2"))) ); // "1", not same "1.2" ⇒ not integer property
+
+
+
+// key are non-integer, then they are listed in the creation order, for instance
+
+let user = {
+    name: "John",
+    surname: "Smith"
+  };
+  user.age = 25; // add one more
+  
+  // non-integer properties are listed in the creation order
+  for (let prop in user) {
+    alert( prop ); // name, surname, age
+  }
