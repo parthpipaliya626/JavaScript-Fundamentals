c79fb5abcdacd9c1d149c57840136ee7054d10ea
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-10 16:53:32 +0530

    Integer properties

---
+++ 4/4.1/task_4.js
@@ -0,0 +1,7 @@
+// Integer properties
+
+// Number(...) explicitly converts to a number
+// Math.trunc is a built-in function that removes the decimal part
+alert( String(Math.trunc(Number("49"))) ); // "49", same, integer property
+alert( String(Math.trunc(Number("+49"))) ); // "49", not same "+49" ⇒ not integer property
+alert( String(Math.trunc(Number("1.2"))) ); // "1", not same "1.2" ⇒ not integer property
