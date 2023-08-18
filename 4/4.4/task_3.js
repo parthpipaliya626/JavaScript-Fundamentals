934c0b3edc8c16065fd0006740cda1c16339be8c
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-18 15:04:07 +0530

    before.dot

--- 4/4.4/task_3.js
+++ 4/4.4/task_3.js
@@ -6,7 +6,7 @@ let user = { name: "Parth" };
 let admin = { name: "Admin" };
 
 function sayHi() {
-    alert( this.name );
+    console.log( this.name );
 }
 
 // use the same function in two objects
@@ -14,4 +14,11 @@ function sayHi() {
 user.f = sayHi;
 admin.f = sayHi;
 
+// tese calls have different this
+//  "this" inside the function is the object "before the dot"
 
+user.f();   // Parth  (this == user)
+admin.f();   // Admin  (this == admin)
+
+
+admin['f']();   // Admin (dot or square brackets access the method – doesn't matter)
