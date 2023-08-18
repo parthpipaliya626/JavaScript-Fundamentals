1a0cb07feba3ea142dfedaff997a1a08a7bd02cf
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-18 15:33:49 +0530

    summary add

--- 4/4.4/object_methods_this.js
+++ 4/4.4/object_methods_this.js
@@ -20,3 +20,16 @@ user.sayHi();
 
 // we have to pre-declared i  this method like,
 // user.sayHI = sayHi then also work same direction
+
+
+
+    // Summary
+    // Functions that are stored in object properties are called “methods”.
+    // Methods allow objects to “act” like object.doSomething().
+    // Methods can reference the object as this.
+    // The value of this is defined at run-time.
+
+    // When a function is declared, it may use this, but that this has no value until the function is called.
+    // A function can be copied between objects.
+    // When a function is called in the “method” syntax: object.method(), the value of this during the call is object.
+    // Please note that arrow functions are special: they have no this. When this is accessed inside an arrow function, it is taken from outside.
