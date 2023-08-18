15db00ecdfa5bd24096978cc14b77e59d63d5a91
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-18 16:39:36 +0530

    requirement install

--- 4/4.4/task_6.js
+++ 4/4.4/task_6.js
@@ -1,3 +1,11 @@
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+
+
+// npm install readline-sync
 const readline = require('readline-sync');
 
 
