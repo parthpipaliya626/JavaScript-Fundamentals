2456f82403234073dff0dc68a9443a3907796c33
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-21 12:20:59 +0530

    more in global symbols

--- 4/4.7/task_4.js
+++ 4/4.7/task_4.js
@@ -1,6 +1,12 @@
 // global symbols
+/*
+     In order to read (create if absent) a symbol from the registry, 
+     use Symbol.for(key).
 
-
+     That call checks the global registry, and if there’s a symbol 
+       described as key, then returns it, otherwise creates a new symbol
+         Symbol(key) and stores it in the registry by the given key.
+*/
 // read from the global registry
 let id = Symbol.for("id"); // if the symbol did not exist, it is created
 
@@ -10,3 +16,5 @@ let idAgain = Symbol.for("id");
 // the same symbol
 console.log( id === idAgain );   // true
 
+
+// Symbols inside the registry are called global symbols. If we want an application-wide symbol, accessible everywhere in the code – that’s what they are for.
