6337a0ebf62cbb78393bf35fe4a50ae6ceccef56
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-23 18:56:20 +0530

    changing the case

---
+++ 5/5.3/task_1.js
@@ -0,0 +1,11 @@
+// changing the case
+
+console.log( 'Interface'.toUpperCase() ); // INTERFACE
+console.log( 'Interface'.toLowerCase() ); // interface
+
+
+console.log( 'Interface'[0].toLowerCase() ); // 'i'
+
+
+
+
