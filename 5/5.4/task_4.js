bee8c3dc90e9235445599fb96ac435ceb324a105
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-24 18:53:14 +0530

    multidimensional arrays

---
+++ 5/5.4/task_4.js
@@ -0,0 +1,17 @@
+// multidimensional arrays
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+  console.log( matrix[0][0] ); // 1, the central element
+  console.log( matrix[0][1] ); // 2, the central element
+  console.log( matrix[0][2] ); // 3, the central element
+  console.log( matrix[1][0] ); // 4, the central element
+  console.log( matrix[1][1] ); // 5, the central element
+  console.log( matrix[1][2] ); // 6, the central element
+  console.log( matrix[2][0] ); // 7, the central element
+  console.log( matrix[2][1] ); // 8, the central element
+  console.log( matrix[2][2] ); // 9, the central element
