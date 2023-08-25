70efd9339c0bcb0937c5115997af44c0c7eebe73
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-25 13:02:15 +0530

    forEach

---
+++ 5/5.5/forEach.js
@@ -0,0 +1,15 @@
+// Iterate: orEach
+
+// The arr.forEach method allows to run a function for every element of the array:
+
+// for each element call console.log
+
+
+// For instance, this shows each element of the array
+["Raju", "Utsav", "Krunal"].forEach(console.log);
+
+
+// And this code is more elaborate about their positions in the target array:
+["Raju", "Utsav", "Krunal"].forEach((item, index, array) => {
+    console.log(`${item} is at index ${index} in ${array}`);
+  });
