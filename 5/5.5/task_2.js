4a0341e01d3eeeb4d048d1a37a2c47485a08eafa
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-25 15:22:13 +0530

    arr.findIndex   &  arr.findLastIndex

--- 5/5.5/task_2.js
+++ 5/5.5/task_2.js
@@ -24,7 +24,21 @@ console.log(user2.name);      // Utsav
 console.log(user3.name);      // Krunal
 
 
+// arr.findIndex
+// arr.findLastIndex
 
+users = [
+    { id: 1, name: "Raju"},
+    { id: 2, name: "Utsav"},
+    { id: 3, name: "Krunal"},
+    { id: 3, name: "Raju"}
+];
+
+// Find the index of the first Raju
+console.log( users.findIndex(user => user.name == 'Raju') );   // 0
+
+// Find the index of the last Raju
+console.log( users.findLastIndex(user => user.name == 'Raju') );  // 3
 
 
 
