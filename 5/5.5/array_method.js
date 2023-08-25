6c8c6ed1f32bf6be11d66211d6c27e63ffd2b0f7
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-25 11:20:14 +0530

    splice method remove element

--- 5/5.5/array_method.js
+++ 5/5.5/array_method.js
@@ -23,23 +23,51 @@ console.log( arr[1] ); // undefined
 console.log( arr.length ); // 3
 
 
-
+// remove [0, 3] means first 3 means (0, 1, 2)  thik haii ??
 
 arr = ["I", "study", "JavaScript"];
 
-arr.splice(2, 2); // from index 1 remove 1 element    [ 'I', 'study' ]
- arr.splice(1, 1); // from index 1 remove 1 element   ["I", "JavaScript"]
+arr.splice(1, 2); // from index 1 remove 1 element    [ 'I' ]
+console.log( arr ); // ["I", "JavaScript"]
+
+arr = ["I", "study", "JavaScript"];
+ arr.splice(0, 2); // from index 1 remove 1 element   [ "JavaScript"]
 
 console.log( arr ); // ["I", "JavaScript"]
 
+console.log( arr.length );     // 2
 
 
 // In the next example we remove 3 elements and replace them with the other two:
  arr = ["I", "study", "JavaScript", "right", "now"];
 
 // remove 3 first elements and replace them with another
-arr.splice(0, 3, "Let's", "dance");
+arr.splice(0, 2, "Let's", "dance", "Raju");
 
 console.log( arr ) // now ["Let's", "dance", "right", "now"]
 
+
+
+
+
+
+// splice returns the array of removed elements
+
+ arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 2 first elements
+let removed = arr.splice(0, 2);
+
+console.log( removed ); // "I", "study" <-- array of removed elements
+
+
+
+ arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+console.log( arr ); // "I", "study", "complex", "language", "JavaScript"
 
