65991bb7e08982b2deea3712d7f69b027214f87c
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-25 11:52:00 +0530

    concat method to add new array

--- 5/5.5/slice_method.js
+++ 5/5.5/slice_method.js
@@ -26,5 +26,25 @@ console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
 
 console.log("concat method start here....");
 arr = [1, 2];
+// create an array from: arr and [3,4]
+console.log( arr.concat([3, 4]) ); // 1,2,3,4
 
-console.log( arr.concat([3, 4]) );     // 1, 2, 3, 4
+// create an array from: arr and [3,4] and [5,6]
+console.log( arr.concat([3, 4], [5, 6]) ); // 1,2,3,4,5,6
+
+// create an array from: arr and [3,4], then add values 5 and 6
+console.log( arr.concat([3, 4], 5, 6) ); // 1,2,3,4,5,6
+
+
+
+
+arr = [1, 2];
+
+let arrayLike = {
+  0: "something",
+  length: 1
+};
+
+console.log( arr.concat(arrayLike) ); // 1,2,[object Object]
+
+
