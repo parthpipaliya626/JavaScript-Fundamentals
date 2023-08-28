db054838c7974f964b96d01e9f36b6e631995246
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-28 19:38:32 +0530

    str.split use in Array.from

--- 5/5.6/task_2.js
+++ 5/5.6/task_2.js
@@ -18,13 +18,40 @@ while (true) {
 // Iterables and array-likes
 
 
-let arrayLike = { // has indexes and length => array-like
+// let arrayLike = { // has indexes and length => array-like
+//     0: "Hello",
+//     1: "World",
+//     length: 2
+//   };
+  
+//   // Error (no Symbol.iterator)
+//   for (let item of arrayLike) {}
+
+
+  // Array.from
+
+  let arrayLike = {
     0: "Hello",
     1: "World",
     length: 2
   };
   
-  // Error (no Symbol.iterator)
-  for (let item of arrayLike) {}
+  let arr = Array.from(arrayLike); // (*)
+  console.log(arr.pop()); // World (method works)
 
-  
+
+//   // assuming that range is taken from the example above
+//  arr = Array.from(range);
+// console.log(arr); // 1,2,3,4,5 (array toString conversion works)  // not defined
+
+// here we use Array.from to a string into an array of char:
+ str = '𝒳😂';
+
+// splits str into array of characters
+let chars = Array.from(str);
+
+console.log(chars[0]); // 𝒳
+console.log(chars[1]); // 😂
+console.log(chars.length); // 2
+
+
