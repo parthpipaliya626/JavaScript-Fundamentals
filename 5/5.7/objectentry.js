9769aa9a844906c3ce666b7f2eada8af7b58091e
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-31 19:37:58 +0530

    set.add(value)

--- 5/5.7/objectentry.js
+++ 5/5.7/objectentry.js
@@ -51,3 +51,49 @@ map.set('meat', 4);
 console.log(obj.orange);    // 2
 
 
+
+// here we make line (*) shorter
+// let obj = Object.fromEntries(map); // omit .entries()
+
+
+
+/* Set
+   A Set is a special type collection – “set of values” (without keys), where each value may occur only once.
+
+  Its main methods are:
+
+        new Set([iterable]) – creates the set, and if an iterable object is provided (usually an array), copies values from it into the set.
+        set.add(value) – adds a value, returns the set itself.
+        set.delete(value) – removes the value, returns true if value existed at the moment of the call, otherwise false.
+        set.has(value) – returns true if the value exists in the set, otherwise false.
+        set.clear() – removes everything from the set.
+        set.size – is the elements count.
+
+
+
+        set.add(value)
+*/
+
+let set = new Set();
+
+let raju = { name: "Raju" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+let marys = { name: "Marys" };
+
+// visits, some users come multiple times
+set.add(raju);
+set.add(pete);
+set.add(mary);
+set.add(raju);
+set.add(raju);
+set.add(mary);
+set.add(marys);
+
+// set keeps only unique values
+console.log( set.size ); // 3
+
+for (let user of set) {
+  console.log(user.name); // Raju (then Pete and Mary)
+}
+
