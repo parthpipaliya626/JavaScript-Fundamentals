3d033422c7f5f5cafbbb8c47c9888927aa4fd9af
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-01 18:02:34 +0530

    Book Rating System:

--- advancetask.js
+++ advancetask.js
@@ -4,6 +4,8 @@
       and use checkoutBook(isbn) to checked out each book 
        and increment the count then MAX_CHECKOUTS limit over then
         don't allow the checkout and inform the user 
+
+
       */
 
 class Book {
@@ -14,6 +16,7 @@ class Book {
         this.isbn = isbn;
         this.checkoutCount = 0;
         this.dueDate = null;      // Initialize dueDate to null 
+        this.ratings = []; // Initialize ratings as an empty array
     }
 
     checkoutBook() {
@@ -35,6 +38,23 @@ class Book {
             return new Date() > new Date(this.dueDate);
         }
         return false;
+    }
+
+    rateBook(rating) {
+        if (rating >= 1 && rating <= 5) {
+            this.ratings.push(rating);    // Add the rating to the ratings array
+            console.log(`Rated '${this.title}' by ${this.author} (ISBN: ${this.isbn}) with ${rating} stars.`);
+        } else {
+            console.log(`Invalid rating. Ratings must be between 1 and 5.`);
+        }
+    }
+
+    getAverageRating() {
+        if (this.ratings.length === 0) {
+            return 0;
+        }
+        const sum = this.ratings.reduce((acc, curr) => acc + curr, 0);
+        return sum / this.ratings.length;   // calculate the average rating
     }
 }
 
@@ -73,13 +93,32 @@ class Library {
         }
         return overdueBooks;
     }
+
+    rateBook(isbn, rating) {
+        if (isbn in this.books) {
+            const book = this.books[isbn];
+            book.rateBook(rating);
+        } else {
+            console.log(`Book with ISBN ${isbn} not found in the library.`);
+        }
+    }
+
+    getAverageRating(isbn) {
+        if (isbn in this.books) {
+            const book = this.books[isbn];
+            return book.getAverageRating();
+        } else {
+            console.log(`Book with ISBN ${isbn} not found in the library.`);
+            return 0;
+        }
+    }
 }
 
 // Usage example:
 const library = new Library();
 
 const book1 = new Book( "Indian fighter", "Khali", "123456789");
-const book2 = new Book( "Fighter & Actor", "Rock", "987654321" );
+const book2 = new Book( "Fighter & Actor", "Rock", "987654321");
 
 library.addBook(book1);
 library.addBook(book2);
@@ -87,13 +126,19 @@ library.addBook(book2);
 book1.checkoutBook(); // Checkout book1
 book1.checkoutBook(); // Checkout book1 again
 
-
-// Simulate an overdue book by setting a past dueDate
-book2.dueDate = new Date("2023-08-01"); // Set a past dueDate for book2
-
-console.log("Overdue Books:");
-const overdueBooks = library.listOverdueBooks();
-overdueBooks.forEach(book => console.log(`'${book.title}' by ${book.author}`));
+book1.rateBook(4); // Rate book1 with 4 stars
+book1.rateBook(5); // Rate book1 with 5 stars
 
 
+// // Simulate an overdue book by setting a past dueDate
+// book2.dueDate = new Date("2023-08-01"); // Set a past dueDate for book2
 
+// console.log("Overdue Books:");
+// const overdueBooks = library.listOverdueBooks();
+// overdueBooks.forEach(book => console.log(`'${book.title}' by ${book.author}`));
+
+
+
+console.log(`Average rating for 'The Great Gatsby': ${library.getAverageRating("123456789").toFixed(2)}`);
+
+console.log(`Average rating for 'To Kill a Mockingbird': ${library.getAverageRating("987654321").toFixed(2)}`);
