57fffb16fe29a8ce20fa2daf40647b723963dea8
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-01 11:42:44 +0530

    update

--- library_management.js
+++ library_management.js
@@ -1,84 +1,153 @@
 /*  Library Management  */
 
-
-// Book Object Creation:
- /* Create a function createBook(title, author, isbn) that takes in a book's title, author, and ISBN and returns an object with the given details and a default checkedOut status of false */
+'use strict';
 
 function createBook (title, author, isbn) {
-  return { title, author, isbn, checkedOut: false, 
-      };
+    return {
+        title,
+        author,
+        isbn,
+        checkedOut: false,
+    };
 }
 
-// Create an array library that will store all the book objects.
-const library = [];
+const book1 = createBook ('Bhagavad Gita', 'Ved Vyas', 100000000001);
+const book2 = createBook ('the Ocean tour', 'Rajeshbhai', 9874567891011);
+const book3 = createBook ('the Great', 'Munnabhai', 6844567891011);
+const book4 = createBook ('Mahabharat', 'Ved Vyas', 445675491011);
 
+/* library array */
 
-// Add books to the library using the createBook function
-library.push(createBook("To Kill a Mockingbird", "Harper Lee", "978-0-06-112008-4"));
-library.push(createBook("1984", "George Orwell", "978-0-452-28423-4"));
-library.push(createBook("The world War", "George Orwell", "978-0-452-28423-3"));
-library.push(createBook("Brave New World", "Aldous Huxley", "978-0-06-085052-4"));
+let library = [];
 
+// library = [createBook ('Mahabharat', 'Ved Vyas', '978-0735219090')];
 
-// add books to the library with addBookToLibrary(book) function 
-function addBookToLibrary(book){
-    library.push(book);
+library = [ 
+    {
+        title: 'Panchtantra',
+        author:'Vishnu Sharma',
+        isbn: 9789380171104,
+        checkedOut: false
+    },
+    {
+        title: 'Shikshapatri',
+        author:'Shree Swaminarayan',
+        isbn: 9788193935439,
+        checkedOut: false
+    },
+];
+
+/* Add Books: */
+function addBookToLibrary (book) {
+  library.push(book);
 }
 
-// function to find a book by its isbn with checkedoutBook(isbn) 
- function findBookByISBN(isbn) {
-    return library.find((book) => book.isbn === isbn);
- }
+addBookToLibrary (book1); //
+addBookToLibrary (book2);
+addBookToLibrary (book3);
+addBookToLibrary (book4);
 
+// console.log (library);       // all library data
 
- // function to check out a book by its ISBN 
- function checkoutBook(isbn) {
-    const book = findBookByISBN(isbn);
+/* Checkout Book:  */
+function checkoutBook(isbn) {
+    const book = library.find((book) => {
+        return (book.isbn === isbn)
+    });
     if (book) {
         book.checkedOut = true;
-        console.log(`Checked out "${book.title}" by ${book.author} `);
+        console.log(`Book with ISBN ${isbn} has been checked out.`);
     } else {
-        console.log(`Book with ISBN ${isbn} not found in the library.`);
+        console.log(`Book with ISBN ${isbn} was not found.`);
     }
- }
+}
+
+// calling function checkoutBook
+// checkoutBook(9788193935439);
+// checkoutBook(100000000001);
+// checkoutBook(445675491011);
 
 
- // Function to return a book by its ISBN
+// library in all book data in table format
+// console.table(library);          // all library data with checkedout status
 
- function returnBook(isbn) {
-    const book = findBookByISBN(isbn);
+/* Return Book: */
+
+function returnBook(isbn) {
+    const book = library.find((book) => {
+        return (book.isbn === isbn)
+    });
     if (book) {
         book.checkedOut = false;
-        console.log(`Returned "${book.title}" by ${book.author}`);
+        console.log(`Book with ISBN ${isbn} has been returned.`);
     } else {
-        console.log(`Book with ISBN ${isbn} not found in the library.`);
+        console.log(`Book with ISBN ${isbn} was not found.`);
     }
- }
+}
 
- // Function to find books by a specific author
- function findBooksByAuthor(author) {
-    return library.filter((book) => book.author === author);
- }
+// calling function returnBook
+// returnBook(9788193935439);
 
-
-
- // Test cases
-// 1, 2 
- addBookToLibrary(createBook("The Great Gatsby", "F. Scott Fitzgerald", "978-0-7432-7356-5"));
-
-// 3
-checkoutBook("978-0-452-28423-4");
-
-// 4
-returnBook("978-0-452-28423-4");
+// library in all book data in table format
+// console.table(library);      // all library data with return or not in table format
+// console.log(library);       // all library data with return or not in json format
 
 
-// 5
+/* Find Book by Author: */
 
-// console.log("Books by George Orwell: ");
-// console.log(findBooksByAuthor("George Orwell"));
+function findBooksByAuthor(author) {
+        return library.filter(book => book.author === author)
+     }
 
-// console.log("Library contents:");
+     const booksByAuthor = findBooksByAuthor("Ved Vyas");   // enter proper name
+    //  console.log(booksByAuthor);    // all data of author's book details in library
+    
+//----------------------------------------------------------------------------------------------------------
+/* Test Cases
+1.) Create a few books using createBook().
+2.) Add these books to the library.
+3.) Check out a few books using their ISBNs.
+4.) Return a book.
+5.) Find all books by a specific author.
+*/
+//----------------------------------------------------------------------------------------------------------
+// case - 1
+console.log("Test Case 1: create a few books using createBook().");
 
-// console.log(library);
+const newbook1 = createBook ('Javascript', 'Patelbhai', 123456);
+const newbook2 = createBook ('NodeJs', 'Rajubhai', 12);
 
+console.log(newbook1);
+console.log(newbook2);
+//----------------------------------
+console.log();
+
+// case - 2
+console.log("Test Case 2: Add these books to the library.");
+
+addBookToLibrary (newbook1);
+addBookToLibrary (newbook2);
+console.table(library);
+// ------------------------------------
+console.log();
+
+// case - 3
+console.log("Test Case 3: Check out a few books using their ISBNs.");
+
+checkoutBook(9788193935439);
+checkoutBook(100000000001);
+/// -------------------------------------
+console.log();
+
+// case - 4
+console.log("Test Case 4: Return a book.");
+
+returnBook(9788193935439);
+returnBook(100000000001);
+/// -------------------------------------
+console.log();
+// case - 5
+console.log("Test Case 5: Find all books by a specific author.");
+
+ console.table(findBooksByAuthor("Ved Vyas")); 
+//----------------------------------------------------------------------------------------------------------
