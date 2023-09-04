fac3ceb05eb640c6e224648ae40a0f1a971a1a1a
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 16:32:39 +0530

    function parameter destructuring

--- 5/5.10/nested.js
+++ 5/5.10/nested.js
@@ -27,4 +27,23 @@ console.log(item2);  // Donut
 
 
 
+// Smart fuunction parameters
+
+
+	
+// we pass object to function
+ options = {
+    title: "My menu",
+    items: ["Item1", "Item2"]
+  };
+  
+  // ...and it immediately expands it to variables
+  function showMenu({title = "Untitled", width = 200, height = 100, items = []}) {
+    // title, items – taken from options,
+    // width, height – defaults used
+    console.log( `${title} ${width} ${height}` ); // My Menu 200 100
+    console.log( items ); // Item1, Item2
+  }
+  
+  showMenu(options);
 
