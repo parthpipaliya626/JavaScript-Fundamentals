211eba70b58901821341ec3df5685aab2759b694
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 14:07:57 +0530

    swap variable trick

--- 5/5.10/task_1.js
+++ 5/5.10/task_1.js
@@ -10,3 +10,15 @@ user.set("age", "21");
 for (let [key, value] of user) {
     console.log(`${key}:${value}`);     // name:Parth    age:21  
 }
+
+
+// swap variable trick
+
+let guest = "Parth";
+let admin = "Raju";
+
+[guest, admin] = [admin, guest];
+
+console.log(`${guest} ${admin}`);
+
+
