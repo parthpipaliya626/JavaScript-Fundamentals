9abb8d3a3de05f380dd6ecaae43f17dd350e3f95
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 19:28:22 +0530

    localstorage is not defined

---
+++ 5/5.11/date.js
@@ -0,0 +1,12 @@
+// Date andd Timee
+
+let now = new Date();
+console.log(now);     // show curent date/time by timezone of your device system
+
+// 0 means 01.01.1970 UTC+0
+let Jan01_1970 = new Date(0);
+console.log( Jan01_1970 );
+
+// now add 24 hours, get 02.01.1970 UTC+0
+let Jan02_1970 = new Date(24 * 3600 * 1000);
+console.log( Jan02_1970 );
