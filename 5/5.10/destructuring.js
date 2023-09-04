09c52048671fbee15798be9b7baf3aa4261b7ecd
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 12:54:35 +0530

    split method to char value

---
+++ 5/5.10/destructuring.js
@@ -0,0 +1,21 @@
+// Destructuring assignment
+
+// Array destructuring
+
+
+let arr = ["Raju", "Utsav", "Krunal"]
+
+let [FirstName, SecondName, LastName] = arr;
+
+console.log(FirstName);
+console.log(SecondName);
+console.log(LastName);
+
+
+
+
+
+let [firstName, surname] = "John Smith".split(' ');
+console.log(firstName); // John
+console.log(surname);  // Smith
+ 
