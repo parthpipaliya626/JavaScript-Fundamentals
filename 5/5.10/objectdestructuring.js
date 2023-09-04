665c86850866b597dcb803d0405db6f71527e707
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 14:41:27 +0530

    properties are assignes

---
+++ 5/5.10/objectdestructuring.js
@@ -0,0 +1,20 @@
+// Object destructuring
+
+
+let options = {
+    title: "Table",
+    width: 100,
+    height: 200
+};
+
+let {title, width, height} = options;
+
+console.log(title);
+console.log(width);
+console.log(height);
+console.log(options.length);
+
+
+
+
+
