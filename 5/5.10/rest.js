826971efd15dabc8e2615fa1fe7ecaaea7cf8955
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 14:34:45 +0530

    readline package to get input

--- 5/5.10/rest.js
+++ 5/5.10/rest.js
@@ -17,6 +17,46 @@ console.log(name2); // Caesar
  [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
 
 // rest is array of items, starting from the 3rd one
+
 console.log(rest[0]); // Consul
 console.log(rest[1]); // of the Roman Republic
 console.log(rest.length); // 2
+
+
+// we can use any other variable name in place of rest, that also work in destructuring assignment
+
+ [name1, name2, ...titles] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
+console.log(titles);
+
+
+
+// default values
+
+
+
+let [name = "Raju", surname = "Thummar"] = ["Utsav"];
+
+console.log(name);
+console.log(surname);
+
+
+
+// runs only prompt for surname
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter your name: ', (name) => {
+  rl.question('Enter your surname: ', (surname) => {
+    rl.close();
+
+    // Process the input
+    name = name || "Julius"; // Default to "Julius" if no input provided
+    console.log("Name:", name);
+    console.log("Surname:", surname);
+  });
+});
+
