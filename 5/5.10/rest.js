c14834f1c390a49294304122fd304095fb717cb9
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-04 14:10:13 +0530

    the rest” using three dots "...":

---
+++ 5/5.10/rest.js
@@ -0,0 +1,22 @@
+// The rest '...'
+
+let [name1, name2] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
+
+console.log(name1); // Julius
+console.log(name2); // Caesar
+// Further items aren't assigned anywhere
+
+
+
+
+
+// the rest” using three dots "...":
+
+
+
+ [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
+
+// rest is array of items, starting from the 3rd one
+console.log(rest[0]); // Consul
+console.log(rest[1]); // of the Roman Republic
+console.log(rest.length); // 2
