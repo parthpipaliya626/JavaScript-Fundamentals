6a7646fcf0d0db7db51f800b3471ef241b4ee367
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-12 15:29:11 +0530

    Json methods

--- 5/5.11/autocorrection.js
+++ 5/5.11/autocorrection.js
@@ -5,8 +5,8 @@
 // example - 1 :
 
 let date = new Date(2013, 0, 32); // 32 Jan 2013 ?!?
-console.log(date);   //  2013-01-31T18:30:00.000Z
-// Really ...is 1st Feb 2013! this type as per javascript.info 
+console.log(date);   //  2013-01-31T18:30:00.000Z       in terminal output
+// 1st Feb 2013! this type as per javascript.info 
 
 
 // example - 2 :
@@ -28,4 +28,53 @@ console.log( date );      // 2015-12-31T18:30:00.000Z
 date.setDate(0); // min day is 1, so the last day of the previous month is assumed
 console.log( date );     // 2015-12-30T18:30:00.000Z
 //  31 Dec 2015   really this output aavvu 
+
+
+
+
+
+// Date to number, date diff
+
+
+let start = new Date(); // start measuring time
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+let end = new Date(); // end measuring time
+console.log( `The loop took ${end - start} ms` );   // The loop took 35 ms
+// The loop took 4 ms          in browser 
+
+
+
+
+
+// Date.now()
+
+
+start = Date.now(); // milliseconds count from 1 Jan 1970
+
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+
+ end = Date.now(); // done
+
+console.log( `The loop took ${end - start} ms` ); // subtract numbers, not dates
+
+
+
+let parth = new Date( Date.parse('2012-01-26T13:51:50.417-07:00') );
+
+console.log(parth);
+
+
+let hour = 11;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18 || isWeekend) {
+  console.log("The office is closed.")
+}
+
 
