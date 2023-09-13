fc705c463618398df23ead3ce61f531e7c4bdc9b
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-13 18:40:32 +0530

    recursion to subnesting

---
+++ 6/6.1/recursion.js
@@ -0,0 +1,70 @@
+// Recursion and stack
+
+function pow(x, n) {
+    let result = 1;
+
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+console.log( pow(3, 3));
+
+
+function pow(x, n) {
+    if (n == 1) {
+        return x;
+    } else {
+        return x * pow(x, n - 1);
+    }
+} 
+
+console.log( pow(2, 1));
+
+
+let company = {
+    sales: [{
+      name: 'John',
+      salary: 1000
+    }, {
+      name: 'Alice',
+      salary: 1600
+    }],
+  
+    development: {
+      sites: [{
+        name: 'Peter',
+        salary: 2000
+      }, {
+        name: 'Alex',
+        salary: 1800
+      }],
+  
+      internals: [{
+        name: 'Jack',
+        salary: 1300
+      }]
+    }
+  };
+
+// console.log(company);
+
+
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    return department.reduce((prev, current) => prev + current.salary, 0);
+  } else {
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep);
+    }
+    return sum;
+  }
+}
+
+console.log(sumSalaries(company));     // 7700
+
+
+
