ce5e467c39b13454c9919a7a05698411fb361026
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-13 16:54:56 +0530

    2*(3) times

--- 6/recursion.js
+++ 6/recursion.js
@@ -1,7 +1,26 @@
 // Recursion and stack
 
+function pow(x, n) {
+    let result = 1;
 
-console.log(1.35.toFixed(1));
-console.log(6.35.toFixed(1));
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+console.log( pow(3, 3));
+
+
+function pow(x, n) {
+    if (n == 1) {
+        return x;
+    } else {
+        return x * pow(x, n - 1);
+    }
+} 
+
+console.log( pow(2, 3));
 
 
