857bc0f20fb884a1f20094e8949fdd1ab53b3462
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-13 19:10:45 +0530

    rest parameters

--- 6/6.2/rest_parameters.js
+++ 6/6.2/rest_parameters.js
@@ -4,3 +4,19 @@
 
 
 
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+console.log( sum (1, 2, 3, 4, 5) );    // 6
+
+
+
+function showName(firstName, lastName, ...titles) {
+   console.log(firstName + ' ' + lastName);
+
+   console.log( titles[0] );
+   console.log( titles[1] );
+}
+
+showName("Raju", "Thummar", "Krunal", "Vora")
