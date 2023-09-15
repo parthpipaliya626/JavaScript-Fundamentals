fa9f293776de6ee506b440fafee932daad3b4e6e
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-15 16:36:40 +0530

    function in if

---
+++ 6/6.3/task_2.js
@@ -0,0 +1,17 @@
+let phrase = "Hello";
+
+if (true) {
+    let user = "Parth";
+
+    function sayHi() {
+        console.log(`${phrase}, ${user}`);
+    }
+}
+
+sayHi();
+
+
+
+// Function in if 
+
+
