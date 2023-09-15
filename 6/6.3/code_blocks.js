3478515cc7a6005837d58e3e2b6f2036f8d6eb65
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-15 11:25:21 +0530

    localstorage

--- 6/6.3/code_blocks.js
+++ 6/6.3/code_blocks.js
@@ -7,3 +7,58 @@
     console.log(message);
 }
 
+
+let message = "hello";
+    console.log(message);
+ message = "good bye";
+    console.log(message);
+
+
+
+    // Nested function
+
+    function sayHiBye(firstName, lastName) {
+        function getFullName() {
+            return firstName + " " + lastName;
+        }
+
+        console.log( "Hello, " + getFullName() );
+        console.log( "Bye, " + getFullName() );
+    }
+
+
+    function makeCounter() {
+        let count = 0;
+
+        return function() {
+            return count++;
+        };
+    }
+    let counter = makeCounter();
+
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());
+
+
+    // lexical environment
+
+
+    const { LocalStorage } = require('node-localstorage');
+    const localStorage = new LocalStorage('./localStorage');
+    
+    clickCounter();
+    
+    function clickCounter() {
+        if (localStorage.getItem('clickcount')) {
+            let clickCount = Number(localStorage.getItem('clickcount'));
+            clickCount++;
+            localStorage.setItem('clickcount', clickCount.toString());
+        } else {
+            localStorage.setItem('clickcount', '1');
+        }
+    
+        console.log("Click count: " + localStorage.getItem('clickcount'));
+    }
+    
