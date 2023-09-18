61380474b18fc3310d76d7a9f8e2f79c322f3de0
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-18 18:43:14 +0530

    sum with closures

--- 6/6.3/task_5.js
+++ 6/6.3/task_5.js
@@ -12,3 +12,16 @@ if (true) {
 }
 
 sayHi();
+
+
+
+
+// example 2:  sum with closures
+function sum(a) {
+    return function(b) {
+        return a + b;
+    };
+}
+
+console.log( sum(1)(2) )
+console.log( sum(5)(-1) );
