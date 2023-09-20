ff47a1e6c83253126e58cdc99e159ed77541f61d
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-20 15:20:36 +0530

    readline for terminal

--- 6/6.6/function_object.js
+++ 6/6.6/function_object.js
@@ -1,5 +1,7 @@
 // The name property
 
+const { question } = require("readline-sync");
+
 
 // console.log(sayHi.name);
 
@@ -9,5 +11,44 @@ let  sayHi = function() {
 
 console.log(sayHi.name);
 
+
+
+// The "length" property
+
+function f1(a) {}
+function f2(a, b) {}
+function many(a, b, c, ...more) {}
+
+console.log(f1.length);
+console.log(f2.length);
+console.log(many.length);
+
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(question, ...handlers) {
+    r1.question(question + ' (yes/no): ', (answer) => {
+        const isYes = answer.toLowerCase().trim() === 'yes';
+
+        for (let handler of handlers) {
+            if (handler.length == 0) {
+                if (isYes) handler();
+        
+            } else {
+                handler(isYes);
+            }
+         }
+
+         r1.close();
+    })
+}
+
+
+ask("Question?", () => console.log('You said yes'),  result => console.log(result));
 
 
---
+++ 6/6.6/task_1.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function ask(question, ...handlers) {
+  rl.question(question + ' (yes/no): ', (answer) => {
+    const isYes = answer.toLowerCase().trim() === 'yes';
+
+    for (let handler of handlers) {
+      if (handler.length === 0) {
+        if (isYes) handler();
+      } else {
+        handler(isYes);
+      }
+    }
+
+    rl.close();
+  });
+}
+
+ask("Question?", () => console.log('You said yes'), result => console.log(result));
