b117535ea82065b66032cfa3634c5334a69918bd
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-21 19:31:29 +0530

    cancelling time out

---
+++ 6/6.8/settimeout.js
@@ -0,0 +1,19 @@
+// setTimeout 
+
+
+
+
+function sayHi(phrase, who) {
+    console.log(phrase + ', ' + who);
+  }
+  
+  setTimeout(sayHi, 1000, "Hello", "Parth");
+
+
+
+
+
+
+setTimeout(() => console.log('Hello'), 1000);
+
+
