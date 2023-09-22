6ff5ec3733054f83099e0b861307b33f907e3153
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-22 15:21:23 +0530

    settimeout mehtod

--- 6/6.8/settimeout.js
+++ 6/6.8/settimeout.js
@@ -1,19 +1,36 @@
-// setTimeout 
+// // setTimeout 
 
 
 
 
-function sayHi(phrase, who) {
-    console.log(phrase + ', ' + who);
-  }
+// function sayHi(phrase, who) {
+//     console.log(phrase + ', ' + who);
+//   }
   
-  setTimeout(sayHi, 1000, "Hello", "Parth");
+//   setTimeout(sayHi, 1000, "Hello", "Parth");
+
+// setTimeout(() => console.log('Hello'), 1000);
+
+
+// // canceling with clearTimeout
+
+// let timerId = setTimeout(() => console.log("never happens"), 1000);
+// console.log(timerId); // timer identifier
+
+// clearTimeout(timerId);
+// console.log(timerId); // same identifier (doesn't become null after canceling)
+
+
+// // setTimeout("console.log('hello darling')", 2000);
+// setTimeout("console.log('Hello')", 1000);
+
+setTimeout( console.log('Hello'), 1000);
 
 
 
+ 
 
 
 
-setTimeout(() => console.log('Hello'), 1000);
 
 
