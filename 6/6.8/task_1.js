d33eb9e1d19b9994112c16e29c5cd929842c52cd
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-22 19:31:40 +0530

    every second print 5 to 10

---
+++ 6/6.8/task_1.js
@@ -0,0 +1,41 @@
+// Write a function printNumbers(from, to) that outputs a number every second,
+//      starting from from and ending with to.
+
+
+// using setinterval
+
+function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      console.log(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  // usage:
+  printNumbers(5, 10);
+
+
+
+
+
+  // using nested settimeout
+
+  function printNumbers(from, to) {
+    let current = from;
+  
+    setTimeout(function go() {
+      console.log(current);
+      if (current < to) {
+        setTimeout(go, 1000);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  // usage:
+  printNumbers(5, 10);
