149dc36ba262d70281a51af74e1f542bc75713df
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-09-27 19:15:45 +0530

    decorators method

---
+++ efrg.js
@@ -0,0 +1,16 @@
+let students = [
+  { firstName: 'Parth', lastName: 'Pipaliya', age: 21, joinedDate: 'july 15, 2023',
+  },
+  { firstName: 'Utsav', lastName: 'Thummar', age: 22, joinedDate: 'January 15, 2023',
+  },
+  { firstName: 'Krunal', lastName: 'Vora', age: 22, joinedDate: 'January 24, 2023',
+  },
+];
+
+students.sort ((a, b) => {
+  return array.forEach(element => {
+    
+  });(a.age - b.age);
+  
+});
+
