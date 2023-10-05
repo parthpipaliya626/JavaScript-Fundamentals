7b8331416aa9aec0652f22a66e6270ac2ecd0008
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-10-05 12:10:51 +0530

    example 2

--- split_join.js
+++ split_join.js
@@ -34,15 +34,32 @@ console.log(x.split(' ').join(' '));              //   h  ell o
 
 
 
-
+/*
 
 let sentence = "Hello my name is Parth Pipaliya";
-let word = sentence.split(" ").join(' ');        // [ 'Hello', 'my', 'name', 'is', 'Parth', 'Pipaliya' ]
+let word = sentence.split(" ").join(' ');        //  Hello my name is Parth Pipaliya
 console.log(word);
-let words = sentence.split("   ");        // [ 'Hello my name is Parth Pipaliya' ]
-console.log(words);
+
+let chars = sentence.split(' ');
+console.log(chars);                             // [ 'Hello', 'my', 'name', 'is', 'Parth', 'Pipaliya' ]
+
+let ArraySentence = sentence.split("   ");      // [ 'Hello my name is Parth Pipaliya' ]
+console.log(ArraySentence);
+
+let word1 = sentence.split(" ").sort();        // [ 'Hello', 'Parth', 'Pipaliya', 'is', 'my', 'name' ]
+console.log(word1);
+
+let word2 = chars.join(' ');
+console.log(word2);                            //   Hello my name is Parth Pipaliya
 
 
+
+let x = sentence.split(' ').sort().join(' ');
+console.log(x);                           // Hello Parth Pipaliya is my name
+
+
+
+*/
 
 
 
