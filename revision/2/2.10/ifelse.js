532126287886edec21e0c1f7a86da71346899673
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-10-06 14:57:01 +0530

    if else condition repeat

---
+++ revision/2/2.10/ifelse.js
@@ -0,0 +1,57 @@
+// conditional branching: if, '?'
+
+
+
+// "if" statement
+
+/*
+let year = prompt('fevgr', '');
+console.log(year);
+
+if (year == 2015)  {
+    console.log('right');
+} else {
+    console.log('false');
+}
+
+  */
+
+// several conditions: "else if"
+
+
+/*
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year < 2015) {
+  console.log( 'Too early...' );
+} else if (year > 2015) {
+  console.log( 'Too late' );
+} else {
+  console.log( 'Exactly!' );
+}
+*/
+
+
+// sem but diff style...
+/*    
+let Parth;
+//  Parth = 2015;
+//  Parth = 2014;
+//  Parth = 2018;
+
+if (Parth == 2015) {
+    console.log("right");
+} else if (Parth < 2015) {
+    console.log("bov velo aavi gyo");
+} else if(Parth > 2015) {
+    console.log("aatlu badhu modu aavay tare..??")
+}
+ else {
+    console.log("saru, aavi to gyo ne...")
+}
+
+*/
+
+
+
+
