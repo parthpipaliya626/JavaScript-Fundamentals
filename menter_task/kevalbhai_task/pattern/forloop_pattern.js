2b1241cac417f0b053199e581c12de46633a1ef1
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-10-17 19:31:46 +0530

    design in this

--- menter_task/kevalbhai_task/pattern/forloop_pattern.js
+++ menter_task/kevalbhai_task/pattern/forloop_pattern.js
@@ -7,7 +7,7 @@ let max = 9;
 for(let i = 1; i <= max; i++) {
     
     let pattern = '';
-    for(let j = 1; j <= max ; j++ ) {
+    for(let j = 1; j <= (max - 1) + 1 ; j++ ) {
         pattern +=  "* "
     }
     console.log(pattern);
