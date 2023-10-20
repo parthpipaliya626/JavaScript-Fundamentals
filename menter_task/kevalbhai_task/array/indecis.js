5f5b2029d236c3e2f49d2cbf893de1b1e2372ad7
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-10-20 12:32:37 +0530

    completed task

--- menter_task/kevalbhai_task/array/indecis.js
+++ menter_task/kevalbhai_task/array/indecis.js
@@ -1,5 +1,5 @@
 /* 
-- Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+- Given an nums of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 - You may assume that each input would have exactly one solution, and you may not use the same element twice.
 - You can return the answer in any order.
 */
@@ -11,26 +11,30 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 */
 
-let nums = [2,7,11,15, "a", "b"];
-let target = 9;
-// target = 13;
-// target = 17;
-// target = 18;
-target = 22;
+// let nums = [2,7,11,15];
+// let target = 9;
+// // target = 13;
+// // target = 17;
+// // target = 18;
+// target = 22;
+
+  /*  // example - 1 : 
 
 for (let i = 0; i <= nums.length; i++) {
   for (let j = 0; j <= nums.length; j++) {
     // if ( (target - nums[i] === nums[j]) && (nums[i] != nums[j]) ) {
     if ( (nums[i] + nums[j] === target) && (nums[i] != nums[j]) )  {
-     let output = [i, j];
+     var output = [i, j];
      console.log(output);
-     return output ;
+     return output;
     }  
   }
-}
+} 
+*/
 
 
-// make a function to easy use
+
+// example 2 : make a function to easy use 
 
  /* 
  
@@ -53,7 +57,30 @@ console.log(output(nums, target));
 
 
 
+// example 3 : with only one loop iteration possible 
+ 
+   let nums = [2, 7, 11, 15]
+  let target = 14;
 
+  let output = function (nums, target) {
+    
+    for (let i = 0; i <= nums.length; i++) { 
+      let nums2 = target - nums[i];
+   
+      
+          if ( nums.includes(nums2) ) {
+          let result = nums.indexOf(nums2)
+          return [i, result] 
+        }  else {
+          return ("two number of sum is not target in our input.");
+        }
+      }
+    }
+    console.log(output(nums, target));
+
+
+
+  
 
 
 
