bd036e68374653de588ad489587a4e68baff88f4
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-12-04 14:52:59 +0530

    zero find in index completed

--- menter_task/kevalbhai_task/pattern/zero_find_metrix.js
+++ menter_task/kevalbhai_task/pattern/zero_find_metrix.js
@@ -1,12 +1,20 @@
 // Write a JS code to find the number of zeros in 2D Matrix
 
 let arr = [[0,1,1],[0,1,0],[1,0,0]];
-// let newaa = arr.split('');
-// console.log(newaa);
 
-  
-let count = [];
-let newarr = arr.includes((element) => {
-    array.indexOf[[element]] == [[0]];
-});
-console.log(newarr);
+  function countMatrix(arr) {
+
+    let zerocount = [];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            // condition here
+            if (arr[i][j] == 0) {
+                zerocount++;
+            }
+        }
+    }
+    return zerocount;
+  }
+
+console.log(countMatrix(arr));
