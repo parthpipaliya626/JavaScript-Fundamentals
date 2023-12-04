8f4399de1f757050a29d42e391b30b8c6f9d7d6a
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-12-04 15:50:33 +0530

    delete all occurence of element in array

--- menter_task/kevalbhai_task/pattern/remove_sem_char.js
+++ menter_task/kevalbhai_task/pattern/remove_sem_char.js
@@ -2,23 +2,11 @@
 
 let array = [1,2,1,2, 11, 11, 111,1,8,8,8,8,"8","a","a","a","@","@"];
 
+    let uniquearray = array.filter((a, b) => array.indexOf(a) === b ) ;
+    console.log(uniquearray);
 
-let newarray = array.filter((char, dup) => array.indexOf(char) === dup);
-console.log(newarray);
-
-//  output in terminal : [
-//   1, 2,   11,  111,
-//   8, '8', 'a', '@'
-// ]
-
-
-
-// output in console : [1, 2, 11, 111, 8, '8', 'a', '@']
-
-
-
-
-
+    // output : 
+    // [ 1, 2,   11,  111, 8, '8', 'a', '@']
 
 
 
