ce4eb56327a9683de94369d39876239ff5336e2f
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-12-04 18:18:00 +0530

    solve given task of dhruvik

---
+++ FreeCodeCamp/Intermediate_algo_scripting/diff_two_array.js
@@ -0,0 +1,67 @@
+// diff two arrays
+
+// solution 1: array.concat(), array.filter(), array.indexOf(), array.lastIndexOf()
+
+
+
+let arr1 = [1, 2, 3, 9, 2, 5, 5]
+let arr2= [8, 7, 3, 2, 7, 5]
+
+/* 
+function diffArray(arr1, arr2) {
+    return arr1.concat(arr2).filter((item, i, arr) => arr.indexOf(item) === arr.lastIndexOf(item) );
+}
+console.log(diffArray(arr1, arr2));
+
+ */
+
+
+// solution 2 : use with array.filter(), array.concat(), array.includes()
+
+/* 
+function diffArray(arr1, arr2) {
+   let filter1 = arr1.filter(item => !arr2.includes(item));
+   let filter2 = arr2.filter(item => !arr1.includes(item));
+   return filter1.concat(filter2);
+}
+console.log(diffArray(arr1, arr2));
+ */
+
+
+// solution 3 : For loop(), indexOf(), Array.push()
+
+/*
+ function diffArray(arr1, arr2) {
+    let diff = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        arr2.indexOf(arr1[i]) === -1 && diff.push(arr1[i]);
+        
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        arr1.indexOf(arr2[i]) === -1 && diff.push(arr2[i]);
+        // console.log(diff);
+    }
+   return diff;
+}
+console.log(diffArray(arr1, arr2));
+ */
+
+
+// solution 4 : array.concat(), array.indexOf(), array.push()
+
+
+
+function diffArray(arr1,arr2) {
+    let arr = arr1.concat(arr2);
+    console.log(arr);
+    let diff = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        (arr1.indexOf(arr[i]) === -1 || arr2.indexOf(arr[i]) === -1) && diff.push(arr[i]);
+    }
+    return diff;
+}
+console.log(diffArray(arr1, arr2));
+
---
+++ menter_task/dhruvik_task/arrayofObject.js
@@ -0,0 +1,12 @@
+//  let students = [ {"name" : "Parth", "Surname": "Pipaliya"}, {"name": "Dhruvik", "Surname": "Gondaliya"} ];
+//  students[1].city = "Gondal";
+ 
+//  console.log(students);
+
+
+
+ let object = {
+    students : [ "Parth", "Dhruvik" ]
+ }
+   object.students.push("Dharmik");
+   console.log(object);
